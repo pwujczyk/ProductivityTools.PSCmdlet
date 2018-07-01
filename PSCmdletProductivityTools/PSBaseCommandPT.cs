ccef6ce0e2746ef77e7f9a6851555bbb54b5946c
pwujczyk <pwujczyk@ecovadis.com>
2018-07-01 21:40:46 +0200

    initial commit

---
+++ PSCmdletProductivityTools/PSBaseCommandPT.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSCmdletProductivityTools
+{
+    public abstract class PSBaseCommandPT
+    {
+        public abstract void ProcessRecord();
+    }
+
+    public abstract class PSBaseCommandPT<CmdletType> : PSBaseCommandPT
+    {
+        protected CmdletType Cmdlet;
+
+        public PSBaseCommandPT(CmdletType cmdletType)
+        {
+            this.Cmdlet = cmdletType;
+        }
+
+        protected abstract bool Condition { get; }
+
+        protected abstract void Invoke();
+
+        protected void InvokeCall(Action a)
+        {
+            a();
+        }
+
+        public override void ProcessRecord()
+        {
+            if (this.Condition)
+            {
+                Invoke();
+            }
+        }
+    }
+
+}
---
+++ PSCmdletProductivityTools/PSCmdletPT.cs
@@ -0,0 +1,68 @@
+﻿using DescriptionLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSCmdletProductivityTools
+{
+    public abstract class PSCmdletPT
+    {
+        [Parameter]
+        [Description("Shows this menu")]
+        public SwitchParameter Help { get; set; }
+
+        private List<PSBaseCommandPT> CommandList = new List<PSBaseCommandPT>();
+
+        protected void AddCommand(PSBaseCommandPT command)
+        {
+            this.CommandList.Add(command);
+        }
+
+        protected void ProcessCommands()
+        {
+            if (Help.IsPresent)
+            {
+                ShowHelp();
+            }
+            else
+            {
+                foreach (var item in CommandList)
+                {
+                    item.ProcessRecord();
+                }
+            }
+        }
+
+        private void ShowHelp()
+        {
+            MethodDescription();
+            PrintPropertiesDescription();
+        }
+
+        private void MethodDescription()
+        {
+            var s = this.GetType();
+            var description = s.Description();
+            string line = $"[{s.Name} - {description}]";
+            Console.WriteLine(line);
+        }
+
+        private void PrintPropertiesDescription()
+        {
+            var s = this.GetType().GetProperties();
+            foreach (var item in s)
+            {
+                if (item.CustomAttributes.Any(x => x.AttributeType.Name == "ParameterAttribute"))
+                {
+                    string description = this.GetType().PropertyDescription(item.Name);
+                    string line = $"{item.Name} - {description}";
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
